8ee90c33381ac6da973ca1780b248599dbd80d15
Julio Aguilar <julioa.m777@hotmail.com>
2022-02-26 09:39:12 -0600

    Prueba 1

---
+++ equiposWebAPi/Controllers/equiposController.cs
@@ -0,0 +1,36 @@
+﻿using equiposWebAPi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace equiposWebAPi.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+
+        private readonly prestamosContext _contexto;
+
+        public equiposController(prestamosContext miContexto)
+        {
+            _contexto = miContexto;
+        }
+
+        [HttpGet]
+        [Route("api/equipos")]
+        public IActionResult Get()
+        {
+            IEnumerable<equipos> equiposList = (from e in _contexto.equipos
+                                                select e);
+
+            if (equiposList.Count()>0)
+            {
+                return Ok(equiposList);
+            }
+            return NotFound(); 
+        }    
+
+    }
+}
---
+++ equiposWebAPi/prestamosContext.cs
@@ -0,0 +1,17 @@
+﻿using equiposWebAPi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace equiposWebAPi
+{
+    public class prestamosContext : DbContext
+    {
+        public prestamosContext(DbContextOptions<prestamosContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<equipos> equipos { get; set; }
+
+    }
+}
